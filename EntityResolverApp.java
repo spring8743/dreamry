78d7d632906ab34e41a3b931718c51cb010d176d
feihong.xu <spring8743@163.com>
2019-06-30 16:00:41 +0800

    Add files via upload

---
+++ EntityResolverApp.java
@@ -0,0 +1,16 @@
+package com.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityResolverApp {
+
+	/**
+	 * Entity Point to start the sprint boot web service application
+	 * @param args
+	 */
+    public static void main(String[] args) {
+        SpringApplication.run(EntityResolverApp.class, args);
+    }
+}
